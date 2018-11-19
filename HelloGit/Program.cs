fdccdf61ef864a3365458f7657605c6f35700636
Poul2017 <po.henriksen@gmail.com>
2018-11-19 14:06:48 +0100

    Added a line

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -11,6 +11,7 @@ namespace HelloGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
+            Console.WriteLine("One more line");
         }
     }
 }
